c018a4e2ca6e1f62bc60b35dad16f758ce436df7
unknown <sashko2b3@gmail.com>
2026-06-11 21:49:46 +0200

    KAN-6 add new hover effect to header buttons

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -46,12 +46,12 @@ export default function Header() {
         </form>
         <ul className="flex gap-4">
           <Link to="/">
-            <li className="hidden sm:inline text-slate-700 hover:underline">
+            <li className="hidden sm:inline text-slate-700 hover:underline hover:text-slate-500">
               Home
             </li>
           </Link>
           <Link to="/about">
-            <li className="hidden sm:inline text-slate-700 hover:underline">
+            <li className="hidden sm:inline text-slate-700 hover:underline hover:text-slate-500">
               About
             </li>
           </Link>
